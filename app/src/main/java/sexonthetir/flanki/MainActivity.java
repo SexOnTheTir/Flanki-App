12d4a36f0f88c9b2288f42210d3c20376109e8ad
Krzysztof Gustalik <k.gustalik@gmail.com>
2017-07-10 23:38:00 +0200

    initial commit

---
+++ app/src/main/java/sexonthetir/flanki/MainActivity.java
@@ -0,0 +1,23 @@
+package sexonthetir.flanki;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String email = intent.getStringExtra("email");
+
+        TextView welcomeText = (TextView) findViewById(R.id.welcomeText);
+        welcomeText.setText("Witaj " + email + "!");
+    }
+}
